9182f011407360064b140d2df35ef31edf2ec868
christian_reynaltt <creynaltt@maristak.net>
2024-01-18 22:50:16 +0100

    Ajsutes de estilos

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
-
 import Card from './Card';
+import "../css/books.css"
 
 function Books({ products }) {
     return (
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -4,22 +4,23 @@ import "../css/filters.css"
 function Filters() {
     return (
         <React.Fragment>
-            <div className="range">
-                <input type="range" name="" value="0" min="0" max="1000" />
-
-            </div >
-            <div>
-                <select
-                    id="countries"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                >
-                    <option selected>Selecciona un género</option>
-                    <option value="all">Todas</option>
-                    <option value="Terror">Terror</option>
-                    <option value="Ciencia ficción">Ciencia ficción</option>
-                    <option value="Zombies">Zombies</option>
-                    <option value="Fantasía">Fantasía</option>
-                </select>
+            <div className="filters">
+                <div className="range">
+                    <input type="range" name="" value="0" min="0" max="1000" />
+                </div >
+                <div>
+                    <select
+                        id="countries"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >
+                        <option selected>Selecciona un género</option>
+                        <option value="all">Todas</option>
+                        <option value="Terror">Terror</option>
+                        <option value="Ciencia ficción">Ciencia ficción</option>
+                        <option value="Zombies">Zombies</option>
+                        <option value="Fantasía">Fantasía</option>
+                    </select>
+                </div>
             </div>
         </React.Fragment>
     )
