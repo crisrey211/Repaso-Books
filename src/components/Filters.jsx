f36915ac9085c48af23f8841207f05b9589cc1ac
christian_reynaltt <creynaltt@maristak.net>
2024-01-18 23:22:53 +0100

    Contexto de filtros añadiso pero sin usar

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,16 +2,30 @@ import React from "react"
 import "../css/filters.css"
 
 function Filters() {
+    const [maxPage, setMaxPage] = React.useState(0)
+    const [genre, setGenre] = React.useState("all")
+
+    const handleRange = (event) => {
+        setMaxPage(event.target.value)
+    }
+
+    const handleGenre = (event) => {
+        setGenre(event.target.value)
+    }
+
     return (
         <React.Fragment>
             <div className="filters">
+                <h1>Filtros</h1>
                 <div className="range">
-                    <input type="range" name="" value="0" min="0" max="1000" />
+                    <input type="range" name="page" value={maxPage} min="0" max="1500" onChange={handleRange} />
+                    <label htmlFor="range">{`${maxPage} páginas`}</label>
                 </div >
                 <div>
                     <select
                         id="countries"
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        onChange={handleGenre}
                     >
                         <option selected>Selecciona un género</option>
                         <option value="all">Todas</option>
